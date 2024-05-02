6a7e0ea59a572b1e92bcfad7531cadc9aad828b0
abe-modyo <102051289+abe-modyo@users.noreply.github.com>
2024-05-02 13:50:38 +0000

    doc: update storybook

--- docs/types/components/SimpleComponent.d.ts
+++ docs/types/components/SimpleComponent.d.ts
@@ -1,5 +1,5 @@
 import { DAlert } from '@dynamic-framework/ui-react';
 import type { ComponentProps } from 'react';
-type Props = Pick<ComponentProps<typeof DAlert>, 'soft'>;
-export declare function SimpleComponent({ soft }: Props): import("react/jsx-runtime").JSX.Element;
+type Props = Pick<ComponentProps<typeof DAlert>, 'soft' | 'type'>;
+export declare function SimpleComponent({ soft, type }: Props): import("react/jsx-runtime").JSX.Element;
 export {};
