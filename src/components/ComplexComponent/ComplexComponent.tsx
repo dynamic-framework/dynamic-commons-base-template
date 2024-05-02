1cc2f0a5a22590d6ddb45d0272a028aa53bcdc2d
Abe <agonzalez@modyo.com>
2024-05-02 16:23:47 -0400

    feat: add element 3

--- src/components/ComplexComponent/ComplexComponent.tsx
+++ src/components/ComplexComponent/ComplexComponent.tsx
@@ -1,6 +1,7 @@
 import { useConsoleLogEffect } from './hooks/useConsoleLogEffect';
 import { Element1 } from './component/Element1';
 import { Element2 } from './component/Element2';
+import { Element3 } from './component/Element3';
 
 export function ComplexComponent() {
   useConsoleLogEffect();
@@ -9,6 +10,7 @@ export function ComplexComponent() {
     <div className="d-flex flex-column gap-2">
       <Element1 />
       <Element2 />
+      <Element3 />
     </div>
   );
 }
