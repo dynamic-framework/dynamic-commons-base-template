1f94ca65dcc7419a5f3a42ca207d0cf492d4071e
Abe <agonzalez@modyo.com>
2024-05-02 23:50:15 -0400

    test: add test to complex and simple component

---
+++ src/components/SimpleComponent.spec.tsx
@@ -0,0 +1,29 @@
+import { render } from '@testing-library/react';
+
+import { SimpleComponent } from '.';
+
+it('should render', () => {
+  const { container } = render(
+    <SimpleComponent />,
+  );
+
+  expect(container).toMatchInlineSnapshot(`
+  <div>
+    <div
+      class="alert alert-info"
+      role="alert"
+      style="--bs-alert-component-separator-opacity: 0.3;"
+    >
+      <i
+        class="d-icon bi bi-info-circle alert-icon"
+        style="--bs-icon-component-size: 1.5rem; --bs-icon-component-loading-duration: 1.8s; --bs-icon-component-padding: 0;"
+      />
+      <div
+        class="alert-text"
+      >
+        Simple Component
+      </div>
+    </div>
+  </div>
+  `);
+});
