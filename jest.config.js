0a524d8cf8cd71bf6ec9276087ac048822ef3ca2
Abe <agonzalez@modyo.com>
2024-10-22 11:03:32 -0300

    chore: adjust jest setup and update df

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,30 @@
+const { createDefaultPreset } = require('ts-jest');
+
 module.exports = {
-  preset: 'ts-jest/presets/js-with-ts',
   verbose: false,
   testEnvironment: 'jsdom',
+  transform: {
+    ...createDefaultPreset().transform,
+  },
   maxWorkers: 4,
   cacheDirectory: '<rootDir>/.jest-cache',
   setupFilesAfterEnv: [
-    '<rootDir>/tests/setupTests.ts',
+    '<rootDir>/node_modules/@dynamic-framework/ui-react/jest/setup.js',
+    '<rootDir>/tests/setup.ts',
   ],
   transformIgnorePatterns: [
+    '<rootDir>/node_modules/',
     '<rootDir>/dist/',
+    '<rootDir>/dist-transpiled/',
   ],
   testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
     '<rootDir>/dist/',
+    '<rootDir>/dist-transpiled/',
   ],
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/tests/__mocks__/fileMock.js',
-    '\\.(css|sass|scss)$': '<rootDir>/tests/__mocks__/styleMock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/node_modules/@dynamic-framework/ui-react/jest/__mocks__/fileMock.js',
+    '\\.(css|sass|scss)$': '<rootDir>/node_modules/@dynamic-framework/ui-react/jest/__mocks__/styleMock.js',
   },
 };
+
