374f0acd69509d4a6107e045a132bdb13699301e
Eduard Baron <ebaron@modyo.com>
2025-07-28 09:17:17 -0500

    chore: migrate setup for storybook to v9

--- stories/components/ScssComponent.stories.tsx
+++ stories/components/ScssComponent.stories.tsx
@@ -1,14 +1,10 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 
 import { ScssComponent } from '../../src';
 
 const meta = {
   title: 'Components/ScssComponent',
   component: ScssComponent,
-  tags: ['autodocs'],
-  parameters: {
-    layout: 'centered',
-  },
 } satisfies Meta<typeof ScssComponent>;
 
 export default meta;
