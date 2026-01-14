8c545428e2a7c47685bd027e72912bd6d8d962b3
Eduard Baron <ebaron@modyo.com>
2026-01-14 12:00:32 -0500

    test: update snapshot formatting in SimpleComponent test

--- src/components/SimpleComponent.spec.tsx
+++ src/components/SimpleComponent.spec.tsx
@@ -8,46 +8,46 @@ it('should render', () => {
   );
 
   expect(container).toMatchInlineSnapshot(`
-  <div>
-    <div
-      class="alert alert-info"
-      role="alert"
+<div>
+  <div
+    class="alert alert-info"
+    role="alert"
+  >
+    <span
+      class="d-icon alert-icon"
     >
-      <span
-        class="d-icon alert-icon"
+      <svg
+        aria-hidden="true"
+        class="lucide lucide-info"
+        fill="none"
+        height="24"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        viewBox="0 0 24 24"
+        width="24"
+        xmlns="http://www.w3.org/2000/svg"
       >
-        <svg
-          aria-hidden="true"
-          class="lucide lucide-info"
-          fill="none"
-          height="24"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          viewBox="0 0 24 24"
-          width="24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle
-            cx="12"
-            cy="12"
-            r="10"
-          />
-          <path
-            d="M12 16v-4"
-          />
-          <path
-            d="M12 8h.01"
-            />
-        </svg>
-      </span>
-      <div
-        class="alert-text"
-      >
-        Simple Component
-      </div>
+        <circle
+          cx="12"
+          cy="12"
+          r="10"
+        />
+        <path
+          d="M12 16v-4"
+        />
+        <path
+          d="M12 8h.01"
+        />
+      </svg>
+    </span>
+    <div
+      class="alert-text"
+    >
+      Simple Component
     </div>
   </div>
-  `);
+</div>
+`);
 });
