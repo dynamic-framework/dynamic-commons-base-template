3bf7c099e7ea804ac53b83ed257865ac53bfcac8
lnazarit <lnazarit@modyo.com>
2026-05-20 09:25:50 -0400

    test: enhance ScssComponent snapshot for improved rendering accuracy

--- src/components/ScssComponent/ScssComponent.spec.tsx
+++ src/components/ScssComponent/ScssComponent.spec.tsx
@@ -8,29 +8,29 @@ it('should render', () => {
   );
 
   expect(container).toMatchInlineSnapshot(`
-  <div>
+<div>
+  <div
+    class="wireframe-red"
+  >
     <div
-      class="wireframe-red"
+      style="width: 300px; aspect-ratio: 4 / 3;"
     >
       <div
-        style="width: 300px;"
-      >
-        <div
-          style="width: 100px;"
-        />
-        <div
-          style="width: 100px;"
-        />
-        <p
-          class="float-end"
-        >
-          hola
-        </p>
-      </div>
-      <div
-        style="width: 100px;"
+        style="width: 100px; aspect-ratio: 1 / 1;"
       />
+      <div
+        style="width: 100px; aspect-ratio: 1 / 1;"
+      />
+      <p
+        class="float-end"
+      >
+        hola
+      </p>
     </div>
+    <div
+      style="width: 100px; aspect-ratio: 1 / 1;"
+    />
   </div>
-  `);
+</div>
+`);
 });
